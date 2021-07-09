a2454115e331a0c79831f5907828fe28b76ed1cb
Alindas <hhrhee@163.com>
2021-07-09 14:47:04 +0800

    3d按钮样式修改

--- src/components/3DButton/index.tsx
+++ src/components/3DButton/index.tsx
@@ -4,6 +4,8 @@ import './index.less';
 const Button_3D =  () => {
   return (
     <button className="pushable">
+      <span className="shadow"></span>
+      <span className="edge"></span>
       <span className="front">
         Push me
       </span>
